0b2647c032beecb4d0f95fdd13b8102748de6c30
fcges <fengrunyuan1234567@126.com>
2025-07-02 21:47:13 -0700

    add more info to lab1

--- src/labs/lab1/index.tsx
+++ src/labs/lab1/index.tsx
@@ -3,6 +3,93 @@ export default function Lab1() {
         <div id="wd-lab1">
             <h2>Lab 1</h2>
             <p>Welcome to Lab 1!</p>
+            <div id="wd-p-tag">
+                <h4>Paragraph Tag</h4>
+                <p id="wd-p-1">
+                    This is the first paragraph. The paragraph tag is used to format
+                    vertical gaps between long pieces of text like this one.
+                </p>
+                <p id="wd-p-2">
+                    This is the second paragraph. Even though there is a deliberate white
+                    gap between the paragraph above and this paragraph, by default
+                    browsers render them as one contiguous piece of text as shown here on
+                    the right.
+                </p>
+                <p id="wd-p-3">
+                    This is the third paragraph. Wrap each paragraph with the paragraph
+                    tag to tell browsers to render the gaps.
+                </p>
+            </div>
+            <div id="wd-lists">
+                <h4>List Tags</h4>
+                <h5>Ordered list tags</h5>
+                How to make pancakes:
+                <ol id="wd-pancakes">
+                    <li>Mix dry ingredients.</li>
+                    <li>Add wet ingredients.</li>
+                    <li>Stir to combine.</li>
+                    <li>Heat a skillet or griddle.</li>
+                    <li>Pour batter onto the skillet.</li>
+                    <li>Cook until bubbly on top.</li>
+                    <li>Flip and cook the other side.</li>
+                    <li>Serve and enjoy!</li>
+                </ol>
+                My favorite recipe:
+                <ol id="wd-donuts">
+                    <li>Prepare the dough.</li>
+                    <li>Shape the donuts.</li>
+                    <li>Fry the donuts.</li>
+                    <li>Glaze the donuts.</li>
+                </ol>
+                <h5>Unordered List Tag</h5>
+                My favorite books (in no particular order)
+                <ul id="wd-my-books">
+                    <li>Dune</li>
+                    <li>Lord of the Rings</li>
+                    <li>Ender's Game</li>
+                    <li>Red Mars</li>
+                    <li>The Forever War</li>
+                </ul>
+                Your favorite books (in no particular order)
+                <ul id="wd-your-books">
+                    <li>The Catcher in the Rye</li>
+                    <li>El ingenioso hidalgo don Quijote de la Mancha</li>
+                </ul>
+            </div>
+            <div id="wd-tables">
+                <h4>Table tag</h4>
+                <table border={1} width="100%">
+                    <thead>
+                        <tr>
+                            <th>Quiz</th>
+                            <th>Topic</th>
+                            <th>Date</th>
+                            <th>Grade</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Q1</td>
+                            <td>HTML</td>
+                            <td>2/3/21</td>
+                            <td>85</td>
+                        </tr>
+                        <tr>
+                            <td>Q2</td>
+                            <td>CSS</td>
+                            <td>2/10/21</td>
+                            <td>90</td>
+                        </tr>
+                        <tr> ... </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan={3}>Average</td>
+                            <td>90</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
         </div>
     );
 }
