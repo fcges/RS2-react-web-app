e7260ce5253b9ffc88f95cf28a035081496effaa
fcges <fengrunyuan1234567@126.com>
2025-07-06 01:38:41 -0700

    fixed local image loading path

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -157,7 +157,7 @@ export default function Lab1() {
                 <br />
                 Loading a local image:
                 <br />
-                <img id="wd-react" src="/images/teslabot.jpg" height="200px" />
+                <img id="wd-teslabot" src="./images/teslabot.jpg" height="200px" />
             </div>
             <div id="wd-forms">
                 <h4>Form elements</h4>
